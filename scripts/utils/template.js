9159a34d31f7bfbb37b7e525d7356d7c99592a72
Craig Pounds <nweaver@windstream.net>
2018-04-19 20:17:09 -0500

    Randomized choices for createMultipleChoice()

--- scripts/utils/template.js
+++ scripts/utils/template.js
@@ -45,6 +45,7 @@ const TEMPLATE = (function() {
   }
 	
   function createMultipleChoice() {
+    DATA.questions[DATA.questionIndex].answers.sort((a, b) => 0.5 - Math.random());
     return DATA.questions[DATA.questionIndex].answers.map(answer => 
       `<li>
 					<input type="radio" id="${answer}" name="answer" required="required">			     
